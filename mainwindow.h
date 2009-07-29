cbefc66fb28a1f98fc22bb5c6fb47fa86417e448
sikusiku <sikusiku@e9a33c06-791d-11de-a588-638aa1453bf3>
2009-07-29 03:13:55 +0000

    implemented: view menu -> move number -> show move number

--- mainwindow.h
+++ mainwindow.h
@@ -74,6 +74,7 @@ private:
 
 private slots:
     // File menu
+    void on_actionShowMoveNumber_triggered();
     void on_actionNew_triggered();
     void on_actionOpen_triggered();
     void on_actionReload_triggered();
