a8ab250d39f810989c3563e5e2a3cec78a8525d8
sikusiku <sikusiku@e9a33c06-791d-11de-a588-638aa1453bf3>
2009-08-05 12:17:28 +0000

    added japanese resource, but translation has not been completed.

--- trunk/main.cpp
+++ trunk/main.cpp
@@ -1,12 +1,25 @@
 #include <QtGui/QApplication>
+#include <QDebug>
+#include <QTranslator>
+#include <QLibraryInfo>
 #include <QTextCodec>
 #include "mainwindow.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QTranslator qtTranslator;
+    qtTranslator.load("qt_" + QLocale::system().name(), QLibraryInfo::location(QLibraryInfo::TranslationsPath));
+    a.installTranslator(&qtTranslator);
+
+    QTranslator myappTranslator;
+    bool aa = myappTranslator.load("mugo." + QLocale::system().name());
+    a.installTranslator(&myappTranslator);
+
     QTextCodec::setCodecForCStrings( QTextCodec::codecForName("UTF-8") );
     QTextCodec::setCodecForTr( QTextCodec::codecForName("UTF-8") );
+
     MainWindow w;
     w.show();
     return a.exec();
