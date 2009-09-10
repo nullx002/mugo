e1b2f92002259b56aff1589de1047ab119de36c9
sikusiku <sikusiku@e9a33c06-791d-11de-a588-638aa1453bf3>
2009-09-10 11:38:25 +0000

    print sgf only active branch.

--- printoptiondialog.cpp
+++ printoptiondialog.cpp
@@ -1,13 +1,33 @@
 #include <QDebug>
+#include <QSettings>
 #include "printoptiondialog.h"
 #include "ui_printoptiondialog.h"
 
 PrintOptionDialog::PrintOptionDialog(QWidget *parent) :
     QDialog(parent),
     m_ui(new Ui::PrintOptionDialog),
-    printOption_(0)
+    printOption_(0),
+    movesPerPage_(0)
 {
     m_ui->setupUi(this);
+
+    QSettings settings;
+
+    QRadioButton* printOptions[] = {
+        m_ui->printActiveBoard,
+        m_ui->printActiveBranch,
+        m_ui->printActiveBranchN,
+        m_ui->printAllBranches,
+        m_ui->printAllBranchesN,
+    };
+    int N = sizeof(printOptions) / sizeof(printOptions[0]);
+    int type = settings.value("print/type").toInt();
+    if (type >= 0 && type < N)
+        printOptions[type]->setChecked(true);
+
+    int movesPerPage = settings.value("print/movesPerPage", 50).toInt();
+    m_ui->activeBranchMovesPerPage->setValue(movesPerPage);
+    m_ui->allBranchMovesPerPage->setValue(movesPerPage);
 }
 
 PrintOptionDialog::~PrintOptionDialog()
@@ -45,4 +65,16 @@ void PrintOptionDialog::accept()
             break;
         }
 
+    if (printOption_ == 2)
+        movesPerPage_ = m_ui->activeBranchMovesPerPage->value();
+    else if (printOption_ == 4)
+        movesPerPage_ = m_ui->allBranchMovesPerPage->value();
+    else
+        movesPerPage_ = 0;
+
+
+    QSettings settings;
+    settings.setValue("print/type", printOption_);
+    if (movesPerPage_ != 0)
+        settings.setValue("print/movesPerPage", movesPerPage_);
 }
